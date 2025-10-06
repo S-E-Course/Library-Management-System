8aa1cfa743c02ba37ea113146b75c9bc6ca367b1
Abdullah Almasri <abdallahalmasri2612@gmail.com>
2025-10-06 19:33:11 +0300

    First Test Case: TestHello

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+	
+	public int add(int x, int y) {
+		return x+y;
+	}
+	
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+	}
+	
+}
---
+++ src/test/java/TestHello.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestHello {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		int i1=6;
+		int i2=7;
+		int er=13;
+		
+		Hello o=new Hello();
+		int ar=o.add(i1,i2);
+		
+		assertTrue(er==ar);
+	}
+
+}
