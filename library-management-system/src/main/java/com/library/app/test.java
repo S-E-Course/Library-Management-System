d4eebda7a5358d0d261eac673731924f5a9e6942
Mosub-Samaneh <mosubms84@gmail.com>
2025-10-14 20:02:54 +0300

    some examples you must see in test class

--- library-management-system/src/main/java/com/library/app/test.java
+++ library-management-system/src/main/java/com/library/app/test.java
@@ -1,16 +1,110 @@
 package com.library.app;
-import com.library.dao.UserDAO;
+import java.util.List;
+
+import com.library.model.Book;
 import com.library.model.User;
+import com.library.service.AdminService;
+import com.library.service.UserService;
 
 public class test {
 
 	public static void main(String[] args) throws Exception {
 		
-		UserDAO ud = new UserDAO();
-		User u = new User();
-		u = ud.findByUsername("mosub");
-		System.out.print(u.getEmail());
+		AdminService a = new AdminService();
+		String username = "mosub";
+		String password = "12345";
+		boolean b = a.login(username, password);
+		if(b) {
+			System.out.println("sucsses");
+		}
+		else {
+			System.out.println("failed");
+		}
 		
+		
+		
+		
+		
+		
+		
+		if(a.isLoggedIn()) {
+			System.out.println("Admin Logged In");
+		}
+		
+		
+		
+		
+		
+		
+		
+		String title = "how to know";
+		String author = "MO Sam";
+		String isbn = "1403529763";
+		//a.addBook(title, author, isbn);
+		
+	    List<Book> list;
+	    list = a.searchBooks(isbn);
+	    System.out.println(list.get(0).getAuthor() + " " +  list.get(0).getTitle());
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+		username = "user";
+		String email = "user@gmail.com";
+		String passwordHash = "123";
+		String role = "user";
+	    //a.addUser(username, email, passwordHash, role);
+	    
+	    
+		
+		
+		
+		
+	    
+	    UserService u = new UserService();
+	    username = "user";
+		password = "123";
+		if(u.login(username, passwordHash)) {
+			System.out.println("User Logged In");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		//u.logout();
+		User user;
+		user = u.getLoggedUser();
+		System.out.println(user.getUsername() + " " + user.getEmail());
+		
+		
+		
+		
+		
+		
+		
+		
+		list = u.searchBooks(author);
+	    System.out.println(list.get(0).getAuthor() + " " +  list.get(0).getTitle());
+	    
+	    
+	    
+	    
+	    
+	    
+		
+		a.logout();
+		
+		if(!a.isLoggedIn()) {
+			System.out.println("Admin Logged Out");
+		}
 	}
 
 }
